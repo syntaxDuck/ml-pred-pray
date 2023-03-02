d7594e0f7807f0052fd220ca1144628981dd126c
Kameron Comer <kam.comer96@gmail.com>
2023-03-01 23:57:36 -0500

    Reworked values for generation

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const pray_count = 2;
-const pred_count = 2;
+const entity_count = 12;
+const food_count = 100;
 
 function setup() {
     createCanvas(800, 600);
     world = new World();
 
     eFact = new entityFactory();
-    pray_objs = eFact.create_pray(pray_count);
-    pred_objs = eFact.create_pred(pred_count);
+    pray_objs = eFact.create_pray(entity_count);
+    pred_objs = eFact.create_pred(entity_count);
+    food_objs = Food.create_food(food_count);
 
     world.add_objects("pray", pray_objs);
     world.add_objects("pred", pred_objs);
+    world.add_objects("food", food_objs);
 }
 
 function draw() {
